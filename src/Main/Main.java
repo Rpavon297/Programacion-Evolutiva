b0f008c4cf56a35bd33c0bb5783c10b060d0259f
Rpbc297 <rpbc297@gmail.com>
2019-03-14 15:03:32 +0100

    Refactorizado

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,11 +1,29 @@
 package Main;
 
-import vista.Vista;
+import vista.VistaGenetico;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		//COMENTARIO DE PRUEBA
-		Vista frame = new Vista();
-		frame.setVisible(true);
+		Scanner scan = new Scanner(System.in);
+		String input = "";
+		boolean seguir = true;
+
+		while(seguir) {
+			System.out.println("Introduzca una opción ([g]enetica,[e]volutiva, [s]alir):");
+
+			input = scan.next();
+
+			if(input.equals("g") || input.equals("genetica")){
+				System.out.println("Cargando...");
+				VistaGenetico frame = new VistaGenetico();
+				frame.setVisible(true);
+				seguir = false;
+			}else if(input.equals("e") || input.equals("evolutiva")){
+				System.out.println("vista evolutiva");
+				seguir = false;
+			}else if(input.equals("s") || input.equals("salir"))
+				seguir = false;
+		}
 	}
 }
