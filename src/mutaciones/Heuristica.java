28174277f7928439e88ea79f7964250298fd83b4
Jesús <jesusgranizo@hotmail.com>
2019-03-27 09:48:02 +0100

    Arreglado fallo Mutacion Heuristica

--- src/mutaciones/Heuristica.java
+++ src/mutaciones/Heuristica.java
@@ -9,6 +9,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Heuristica extends Mutacion{
+
+    private Individuo mejor;
+
     @Override
     public void mutar(List<Individuo> poblacion, List<Double> params) {
 
@@ -32,11 +35,11 @@ public class Heuristica extends Mutacion{
                     pos3 = (int) (Math.random() * size);
                 } while (pos3 == 0 || pos3 == size - 1 || pos3 == pos1 || pos3 == pos2);
 
+                this.mejor = new Individuo(new ArrayList<>(ind.getGenes()));
                 List<Integer> posiciones = new ArrayList<>();
                 posiciones.add(pos1);
                 posiciones.add(pos2);
                 posiciones.add(pos3);
-
 
                 List<Gen> conjunto = new ArrayList<>();
                 conjunto.add(ind.getGenes().get(pos1));
@@ -45,49 +48,35 @@ public class Heuristica extends Mutacion{
 
                 List<Gen> resultado = new ArrayList<>();
 
-                List<Gen> indCopy = new ArrayList<>(ind.getGenes());
-                Funcion f = new FuncionViajante();
-                System.out.println(ind.getGenes());
-                System.out.println(f.ejecutar(ind.getFenotipo()));
-                Individuo maximo = new Individuo(new ArrayList<>(ind.getGenes()));
-                permutaciones(indCopy, conjunto, resultado, posiciones, maximo);
-                System.out.println(ind.getGenes());
-                System.out.println(f.ejecutar(ind.getFenotipo()));
+                Individuo indCopy = new Individuo(new ArrayList<>(ind.getGenes()));
+
+                permutaciones(indCopy, conjunto, resultado, posiciones);
+                ind = this.mejor;
             }
         }
         this.nPoblacion = poblacion;
 
     }
 
-    public static List<Gen> permutaciones(List<Gen> individuo, List<Gen> conjunto, List<Gen> resultado, List<Integer> posiciones, Individuo maximo) {
+    private void permutaciones(Individuo individuo, List<Gen> conjunto, List<Gen> resultado, List<Integer> posiciones) {
 
         Funcion f = new FuncionViajante();
 
-        if (conjunto.size()==0) {
-            individuo.set(posiciones.get(0), resultado.get(0));
-            individuo.set(posiciones.get(1), resultado.get(1));
-            individuo.set(posiciones.get(2), resultado.get(2));
-            Individuo i = new Individuo(individuo);
-            return i.getGenes();
-            //double f1 = f.ejecutar(maximo.getFenotipo());
-            //double f2 = f.ejecutar(i.getFenotipo());
-            //System.out.println(posiciones);
-            //if(f1 > f2) {
-                //maximo = new Individuo(new ArrayList<>(i.getGenes()));
-            //}
+        if (conjunto.size()==0)
+        {
+            individuo.getGenes().set(posiciones.get(0), resultado.get(0));
+            individuo.getGenes().set(posiciones.get(1), resultado.get(1));
+            individuo.getGenes().set(posiciones.get(2), resultado.get(2));
+            if(f.ejecutar(individuo.getFenotipo()) < f.ejecutar(this.mejor.getFenotipo()))
+                this.mejor = new Individuo(new ArrayList<>(individuo.getGenes()));
         }
-        for (int i=0; i<conjunto.size(); i++) {
+        for (int i=0;i<conjunto.size();i++)
+        {
             Gen b = conjunto.remove(i);
             resultado.add(b);
-            List<Gen> result = permutaciones(individuo, conjunto, resultado, posiciones, maximo);
-            double f1 = f.ejecutar(maximo.getFenotipo());
-            double f2 = f.ejecutar(new Individuo(new ArrayList<>(result)).getFenotipo());
-            System.out.println(posiciones);
-            if(f1 > f2)
-                maximo = new Individuo(new ArrayList<>(result));
+            permutaciones (individuo, conjunto, resultado, posiciones);
             resultado.remove(b);
-            conjunto.add(i, b);
+            conjunto.add(i,b);
         }
-        return maximo.getGenes();
     }
 }
